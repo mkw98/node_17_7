e5eb055fac382611a5c1e70227e3bc3351f3b4a1
Maria Wieckowska <mkw98@gazeta.pl>
2018-09-23 20:43:20 +0200

    add files edit files

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 var fs = require('fs');
-var StatMode = require('stat-mode');
+var colors = require('colors');
 
-fs.stat('./cat.jpg', function(err, stats) {
-    var statMode = new StatMode(stats);
-    console.log(statMode.toString());
+fs.readFile('./text.txt', 'utf-8', function(err, data) {
+    console.log('Dane przed zapisem!'.blue);
+    console.log(data);
+    fs.appendFile('./text.txt', '\nA tak wyglądają po zapisie!', function(err) {
+        if (err) throw err;
+        console.log('Zapisano!'.blue);
+        fs.readFile('./text.txt', 'utf-8', function(err, data) {
+            console.log('Dane po zapisie'.blue)
+            console.log(data);
+        });
+    });
 });
