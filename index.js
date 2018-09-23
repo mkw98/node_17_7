c818caafc5dcadfbd7f28410ba581f225b172e4b
Maria Wieckowska <mkw98@gazeta.pl>
2018-09-23 21:58:23 +0200

    edit index.js

--- index.js
+++ index.js
@@ -7,17 +7,3 @@ fs.readdir('./', function(err, files) {
 		console.log('Utworzono listę plików');
 	});
 });
-
-
-//fs.readFile('./text.txt', 'utf-8', function(err, data) {
-//    console.log('Dane przed zapisem!'.blue);
-//    console.log(data);
-//    fs.appendFile('./text.txt', '\nA tak wyglądają po zapisie!', function(err) {
-//        if (err) throw err;
-//        console.log('Zapisano!'.blue);
-//        fs.readFile('./text.txt', 'utf-8', function(err, data) {
-//            console.log('Dane po zapisie'.blue)
-//            console.log(data);
-//        });
-//    });
-//});
