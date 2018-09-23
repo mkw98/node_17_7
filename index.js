fbab55334de51db37c71dcacb634e164b9ac7b0b
Maria Wieckowska <mkw98@gazeta.pl>
2018-09-23 21:54:49 +0200

    edit index.js

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 var fs = require('fs');
-var colors = require('colors');
 
-fs.readFile('./text.txt', 'utf-8', function(err, data) {
-    console.log('Dane przed zapisem!'.blue);
-    console.log(data);
-    fs.appendFile('./text.txt', '\nA tak wyglądają po zapisie!', function(err) {
-        if (err) throw err;
-        console.log('Zapisano!'.blue);
-        fs.readFile('./text.txt', 'utf-8', function(err, data) {
-            console.log('Dane po zapisie'.blue)
-            console.log(data);
-        });
-    });
+fs.readdir('./', function(err, files) {
+	if(err) throw err;
+	fs.writeFile('./result.txt', files, function(err){
+		if (err) throw err;	
+		console.log('Utworzono listę plików');
+	});
 });
+
+
+//fs.readFile('./text.txt', 'utf-8', function(err, data) {
+//    console.log('Dane przed zapisem!'.blue);
+//    console.log(data);
+//    fs.appendFile('./text.txt', '\nA tak wyglądają po zapisie!', function(err) {
+//        if (err) throw err;
+//        console.log('Zapisano!'.blue);
+//        fs.readFile('./text.txt', 'utf-8', function(err, data) {
+//            console.log('Dane po zapisie'.blue)
+//            console.log(data);
+//        });
+//    });
+//});
